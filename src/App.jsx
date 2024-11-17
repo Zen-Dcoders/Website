e405fe58fd9427aa133f61e2fe0735b1cbfcb8d4
Zen-Dcoders <admin@zendcoders.in>
2024-11-17 17:18:14 +0530

    Router Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
